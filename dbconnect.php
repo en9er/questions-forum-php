6534586bafa444c34eaea88bd1c468cd91720c27
Rinat Kurbanov <rkurbanov@cloudlinux.com>
2022-02-20 20:40:12 +0500

    db connection added

---
+++ dbconnect.php
@@ -0,0 +1,15 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+try {
+    $conn = new PDO("mysql:host=$_ENV[DB_HOST]; dbname=$_ENV[DB]", $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
